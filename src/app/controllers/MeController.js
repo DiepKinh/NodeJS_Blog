25d71179e1ce76d9265281b7c041dcb1042dd7a4
DiepKinh <thudiep1122pk@gmail.com>
2021-08-23 13:26:24 +0700

    Bổ sung chức năng xóa mềm, thêm cái đếm thùng rác, ẩn khi thùng rác trống, chỉnh time xóa, tham khảo thư viên mongooes delete

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -7,13 +7,29 @@ const {
 class MeController {
   // [GET] /me/stored/cources
   storedCources(req, res, next) {
-    Cource.find({})
-      .then((cources) => {
+    Promise.all([Cource.find({}), Cource.countDocumentsDeleted()])
+      .then(([cources, deletedCource]) => {
         res.render("me/stored-cources", {
           cources: mutipleMongooseToObject(cources),
+          deletedCource, //deletedCource: deletedCource
         });
       })
       .catch((err) => next(err));
+
+    // Gộp 2 promise dưới thành cái trên
+    // Cource.find({})
+    //   .then((cources) => {
+    //     res.render("me/stored-cources", {
+    //       cources: mutipleMongooseToObject(cources),
+    //     });
+    //   })
+    //   .catch((err) => next(err));
+
+    // Cource.countDocumentsDeleted()
+    //   .then((deletedCource) => {
+    //     console.log(deletedCource);
+    //   })
+    //   .catch((err) => next(err));
   }
 
   // [GET] /me/trash/cources
